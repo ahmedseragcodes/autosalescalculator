f097c3917a2ce3d655952c3e2fbac8dc783232ef
ahmedseragcodes <ahmedserag1@gmail.com>
2021-10-07 12:33:47 -0400

    looks like bulma imports aren't working, working on getting project to working foundation to begin redux store creation

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = props => {
   return (
     <>
       <figure className="image is-128x128">
-        <img src={props.car.image} alt={props.car.name} />
+        <img className="carImage" src={props.car.image} alt={props.car.name} />
       </figure>
       <h2>{props.car.name}</h2>
       <p>Amount: ${props.car.price}</p>
