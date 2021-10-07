16b06377665e8f1619e7634853d6a06d0d00a42e
ahmedseragcodes <ahmedserag1@gmail.com>
2021-10-07 12:06:28 -0400

    initial file and dependency setup

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bulma/css/bulma.css';
+import './App.scss';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
